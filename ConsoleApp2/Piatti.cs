2564c33690cb3032765e8be53c62533c4b158119
Mariline Luccisano <y.marilineluccisano@outlook.it>
2024-02-12 21:24:54 +0100

    Soluzione con switch

---
+++ ConsoleApp2/Piatti.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Piatto
+    {
+
+        private string _name { get; set; }
+        private double _prezzo {  get; set; }
+        public Piatto(string nome, double prezzo) {
+
+            this._name = nome;
+            this._prezzo = prezzo;
+        
+        } 
+
+    }
+}
---
+++ ConsoleApp2/menu.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class menu
+    {
+        // --  Il sistema dovrà consentire la selezione di un numero
+        //  indefinito di cibi.L'ultima opzione invece dovrà riportare l'intero
+        //    menu scelto dall'utente, con i relativi prezzi, ed il costo finale,
+        //      comprensivo di servizio al tavolo di € 3.00.
+        static public void MenuPiatti()
+        {
+
+            Console.WriteLine("==============MENU'==============");
+            Console.WriteLine("1: Coca Cola 150 ml (€ 2.50)");
+            Console.WriteLine("2: Insalata di pollo (€ 5.20)");
+            Console.WriteLine("3: Pizza Margherita (€ 10.00)");
+            Console.WriteLine("4: Pizza 4 Formaggi (€ 12.50)");
+            Console.WriteLine("5: Pz patatine fritte (€ 3.50)");
+            Console.WriteLine("6: Insalata di riso (€ 8.00)");
+            Console.WriteLine("7: Frutta di stagione (€ 5.00)");
+            Console.WriteLine("8: Pizza fritta (€ 5.00)");
+            Console.WriteLine("9: Piadina vegetariana (€ 6.00)");
+            Console.WriteLine("10: Panino Hamburger (€ 7.90)");
+            Console.WriteLine("\n11: Stampa conto finale e conferma\n");
+            Console.WriteLine("==============MENU'==============");
+
+        }
+
+
+
+
+
+
+
+
+    }
+    
+}
